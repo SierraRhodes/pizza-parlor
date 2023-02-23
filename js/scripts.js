5e266d6a1e7d30a8ef9836695d20869f6dc8fd81
lilbitsnpieces <alreadywrote@gmail.com>
2023-02-23 13:21:07 -0800

    Add event listeners

--- js/scripts.js
+++ js/scripts.js
@@ -17,9 +17,36 @@ Pizza.prototype.cost = function() {
 
 const toppingCostPerTopping = 1;
 
-
 const standardCost = standardCosts[this.size];
 const toppingCost = toppingCostPerTopping * this.toppings.length;
 
 return standardCost * toppingCost;
 };
+
+const ToppingOptions = [
+  {name: "Pepperoni", price: 1 },
+  {name: "Sausage", price: 1 },
+  {name: "Ham", price: 1 },
+  {name: "Banana Peppers", price: 1 },
+  {name: "Green Peppers", price: 1 },
+  {name: "Spinach", price: 1 },
+  {name: "Mushrooms", price: 1 },
+  {name: "Cheese", price: 1 },
+
+];
+
+//UI Logic 
+
+
+
+
+
+window.addEventListener("load", function(event) {
+
+const form = document.getElementById("pizza-form");
+const total = document.getElementById("total-cost");
+form.addEventListener("submit", function(event) {
+ event.preventDefault();
+});
+
+});
